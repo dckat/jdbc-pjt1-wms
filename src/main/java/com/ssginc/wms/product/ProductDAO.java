887675dc4ba413f9b9ab8f55969904f857d3f581
Kim Daecheol <kdc9619@naver.com>
2024-11-30 21:55:10 +0900

    feat: 상품 검색 메소드 구현
    - 검색 키워드에 따라 상품검색이 되도록 DAO 클래스 내 메소드 구현

--- src/main/java/com/ssginc/wms/product/ProductDAO.java
+++ src/main/java/com/ssginc/wms/product/ProductDAO.java
@@ -41,4 +41,34 @@ public class ProductDAO {
             throw new RuntimeException(e);
         }
     }
+
+    // 키워드에 따라 상품 검색
+    // 검색어에 포함된 모든 물품 검색
+    public ArrayList<UserProductVO> searchProductByKeyword(String selected, String keyword) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT product_id, product_name, " +
+                     "c.category_id category_id, category_name, ord_price, product_amount\n" +
+                     "FROM product p\n" +
+                     "INNER JOIN product_category c\n" +
+                     "ON p.category_id = c.category_id\n" +
+                     "WHERE " + selected + " like ?")) {
+            ps.setString(1, "%" + keyword + "%");
+            try (ResultSet rs = ps.executeQuery()) {
+                ArrayList<UserProductVO> list = new ArrayList<>();
+                while (rs.next()) {
+                    UserProductVO vo = new UserProductVO();
+                    vo.setProductId(rs.getInt("product_id"));
+                    vo.setProductName(rs.getString("product_name"));
+                    vo.setCategoryCode(rs.getInt("category_id"));
+                    vo.setCategoryName(rs.getString("category_name"));
+                    vo.setOrderPrice(rs.getInt("ord_price"));
+                    vo.setProductAmount(rs.getInt("product_amount"));
+                    list.add(vo);
+                }
+                return list;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
