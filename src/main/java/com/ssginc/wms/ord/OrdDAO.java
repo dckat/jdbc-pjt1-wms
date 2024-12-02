ab8cbbb2969f6d6f1e9f876f1b894c5dc3b2ec44
Kim Daecheol <kdc9619@naver.com>
2024-12-02 19:54:17 +0900

    feat: 주문내역 조회 메소드 구현
    - DAO 클래스내 주문 내역 조회를 위한 메소드 구현
    - 로그인한 사용자 기준 주문내역 필터링
    - 기간별 주문내역 필터링 (1주. 1개월. 3개월)

--- src/main/java/com/ssginc/wms/ord/OrdDAO.java
+++ src/main/java/com/ssginc/wms/ord/OrdDAO.java
@@ -3,10 +3,8 @@ package com.ssginc.wms.ord;
 import com.ssginc.wms.hikari.HikariCPDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class OrdDAO {
     private final DataSource dataSource;
@@ -25,6 +23,87 @@ public class OrdDAO {
             ps.setInt(3, vo.getOrderAmount());
             ps.setTimestamp(4, Timestamp.valueOf(vo.getOrderTime()));
             ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<OrdProductVO> listByUserId(String userId) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT ord_id, p.product_id product_id, product_name, " +
+                     "p.category_id category_id, category_name, ord_price, ord_amount, " +
+                     "(ord_price * ord_amount) total_price, " +
+                     "ord_time, ord_status " +
+                     "FROM product p " +
+                     "INNER JOIN ord o " +
+                     "ON p.product_id = o.product_id " +
+                     "INNER JOIN product_category pc " +
+                     "ON p.category_id = pc.category_id " +
+                     "WHERE user_id = ? " +
+                     "ORDER BY ord_time DESC")) {
+            ps.setString(1, userId);
+            try (ResultSet rs = ps.executeQuery()) {
+                ArrayList<OrdProductVO> list = new ArrayList<>();
+                while (rs.next()) {
+                    OrdProductVO vo = new OrdProductVO();
+                    vo.setOrderId(rs.getInt("ord_id"));
+                    vo.setProductId(rs.getInt("product_id"));
+                    vo.setProductName(rs.getString("product_name"));
+                    vo.setCategoryId(rs.getInt("category_id"));
+                    vo.setCategoryName(rs.getString("category_name"));
+                    vo.setOrderPrice(rs.getInt("ord_price"));
+                    vo.setOrderAmount(rs.getInt("ord_amount"));
+                    vo.setTotalPrice(rs.getInt("total_price"));
+                    vo.setOrderTime(rs.getTimestamp("ord_time").toLocalDateTime());
+                    vo.setOrderStatus(rs.getString("ord_status"));
+                    list.add(vo);
+                }
+                return list;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<OrdProductVO> listByPeriod(String userId, String mode) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT ord_id, p.product_id product_id, product_name, " +
+                     "p.category_id category_id, category_name, ord_price, ord_amount, " +
+                     "(ord_price * ord_amount) total_price, " +
+                     "ord_time, ord_status " +
+                     "FROM product p " +
+                     "INNER JOIN ord o " +
+                     "ON p.product_id = o.product_id " +
+                     "INNER JOIN product_category pc " +
+                     "ON p.category_id = pc.category_id " +
+                     "WHERE ord_time BETWEEN SUBDATE(NOW(), INTERVAL " +
+                     switch(mode) {
+                         case "1week" -> "1 WEEK) ";
+                         case "1month" -> "1 MONTH) ";
+                         case "3months" -> "3 MONTH) ";
+                         default -> null;
+                     } +
+                     "AND NOW() and user_id = ? " +
+                     "ORDER BY ord_time DESC")) {
+            ps.setString(1, userId);
+            try (ResultSet rs = ps.executeQuery()) {
+                ArrayList<OrdProductVO> list = new ArrayList<>();
+                while (rs.next()) {
+                    OrdProductVO vo = new OrdProductVO();
+                    vo.setOrderId(rs.getInt("ord_id"));
+                    vo.setProductId(rs.getInt("product_id"));
+                    vo.setProductName(rs.getString("product_name"));
+                    vo.setCategoryId(rs.getInt("category_id"));
+                    vo.setCategoryName(rs.getString("category_name"));
+                    vo.setOrderPrice(rs.getInt("ord_price"));
+                    vo.setOrderAmount(rs.getInt("ord_amount"));
+                    vo.setTotalPrice(rs.getInt("total_price"));
+                    vo.setOrderTime(rs.getTimestamp("ord_time").toLocalDateTime());
+                    vo.setOrderStatus(rs.getString("ord_status"));
+                    list.add(vo);
+                }
+                return list;
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
