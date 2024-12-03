249b3b24c808a21ad6ce13108665494f8666dd8f
gunny <gunny97h@gmail.com>
2024-12-03 12:49:17 +0900

    feat: SupplyService 클래스 추가
    - supplyId를 인코딩 및 디코딩하는 기능 구현
    - int 타입의 ID를 String으로 변환하여 앞에 'S'를 붙이고, 4자리 형식으로 맞춤
    - String 타입의 ID에서 숫자 부분을 추출하여 int로 변환하는 기능 포함

---
+++ src/main/java/com/ssginc/wms/supply/SupplyService.java
@@ -0,0 +1,32 @@
+
+package com.ssginc.wms.supply;
+
+public class SupplyService {
+    // int --> String
+    public static String encondSupplyId(int supplyId) {
+        StringBuilder sTemp = new StringBuilder();
+        sTemp.append("S");
+        String convertedId = String.valueOf(supplyId);
+
+        for (int i = 0; i < 4 - convertedId.length(); i++) {
+            sTemp.append("0");
+        }
+        sTemp.append(convertedId);
+
+        return sTemp.toString();
+    }
+    // String --> int
+    public static int decodeSupplyId(String supplyId) {
+        // 앞에서 부터 0이 나오지 않는 시점을 찾음
+        // 주문 번호는 1부터 auto_increment 하므로 0이 아닌 문자가 포함됨
+        // 아닌 위치부터 끝까지 잘라서 int로 변환하여야 함.
+        for (int i = 0; i < supplyId.length(); i++) {
+            char c = supplyId.charAt(i);
+            //
+            if (Character.isDigit(c) && c > '0' && c <= '9') {
+                return Integer.parseInt(supplyId.substring(i));
+            }
+        }
+        return 0;
+    }
+}
