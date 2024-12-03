c95a8ab72dfc6962f57cb1c1ffe9fd420b3ad927
Kim Daecheol <kdc9619@naver.com>
2024-12-03 12:05:15 +0900

    feat: 주문코드 변환 메소드 구현
    - 직관적인 주문코드 표현을 위한 encode 메서드 구현
    - 주문코드를 DB의 PK로 바꾸기 위한 decode 메서드 구현

--- src/main/java/com/ssginc/wms/ord/OrdService.java
+++ src/main/java/com/ssginc/wms/ord/OrdService.java
@@ -9,7 +9,7 @@ public class OrdService {
     public static int[] getOrdIds(int[] rows, TableModel data) {
         int[] result = new int[rows.length];
         for (int i = 0; i < rows.length; i++) {
-            result[i] = Integer.parseInt(data.getValueAt(rows[i], 0).toString());
+            result[i] = decodeOrderId(data.getValueAt(rows[i], 0).toString());
         }
         return result;
     }
@@ -21,5 +21,29 @@ public class OrdService {
             }
         }
         return true;
+    }
+
+    public static String encondOrderId(int ordId) {
+        StringBuilder sTemp = new StringBuilder();
+        sTemp.append("O");      // 주문코드는 알파벳 O로 시작
+        String convertedId = String.valueOf(ordId);
+
+        // 자릿수 통합을 위해 0추가 (코드는 4자리로 구성)
+        for (int i = 0; i < 4 - convertedId.length(); i++) {
+            sTemp.append("0");
+        }
+        sTemp.append(convertedId);
+
+        return sTemp.toString();
+    }
+
+    public static int decodeOrderId(String orderId) {
+        for (int i = 0; i < orderId.length(); i++) {
+            char c = orderId.charAt(i);
+            if (Character.isDigit(c) && c > '0' && c < '9') {
+                return Integer.parseInt(orderId.substring(i));
+            }
+        }
+        return 0;
     }
 }
