d745e7b9b2a6d1cc6b625146448627749c856bf2
ParkHeonWoo <phwoo1315@naver.com>
2024-12-03 15:10:02 +0900

    chore: 출고 완료 시간 변수 수정
    - 테이블 재생성으로 인해 변수 이름이 일부 바뀜
    - ordCompletionTime -> ordCompleteTime 수정

--- src/main/java/com/ssginc/wms/inoutcomeManagement/OutComeProductVO.java
+++ src/main/java/com/ssginc/wms/inoutcomeManagement/OutComeProductVO.java
@@ -13,7 +13,7 @@ public class OutComeProductVO {
     private int ordId; // 출고 코드
     private int ordAmount; // 출고 수량
     private int productId; // 상품 코드
-    private LocalDateTime ordCompletionTime; // 출고 완료 시간
+    private LocalDateTime ordCompleteTime; // 출고 완료 시간
     private String productName; // 상품 이름
     private int ordPrice; // 출고 단가
     private String categoryName; // 카테고리 이름
